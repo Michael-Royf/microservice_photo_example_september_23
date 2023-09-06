e62f9258e5d893134013d8e8b191f6b4c7bbae1c
Michael-Royf <81159914+Michael-Royf@users.noreply.github.com>
2023-09-06 18:22:05 +0300

    update

---
+++ photos/account_service_photos/src/main/java/com/michael/accountService/AccountServiceApp.java
@@ -0,0 +1,11 @@
+package com.michael.accountService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountServiceApp.class, args);
+    }
+}
